50bfefc944d76cd1d45797786b1f7cb8f18ba610
paullxq2002 <110312084+paullxq2002@users.noreply.github.com>
2025-03-28 20:59:43 +0800

    config.rs

--- src/config.rs
+++ src/config.rs
@@ -98,8 +98,8 @@ const CHARS: &[char] = &[
     'm', 'n', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 ];
 
-pub const RENDEZVOUS_SERVERS: &[&str] = &["rs-ny.rustdesk.com"];
-pub const RS_PUB_KEY: &str = "OeVuKk5nlHiXp+APNn0Y3pC1Iwpwn44JGqrQCsWqmBw=";
+pub const RENDEZVOUS_SERVERS: &[&str] = &["rd.sdren.top"];
+pub const RS_PUB_KEY: &str = "84AOCN8NI2gQZ3z79F3jfcIhqC2Pp4bxx4YRhCu3ftk=";
 
 pub const RENDEZVOUS_PORT: i32 = 21116;
 pub const RELAY_PORT: i32 = 21117;
